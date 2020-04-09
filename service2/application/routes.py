7aae3d0db6b7cc641f1375aa180295955772468a
sarasiraj2009 <45090676+sarasiraj2009@users.noreply.github.com>
2020-04-09 14:41:23 +0100

    Update routes.py

--- service2/application/routes.py
+++ service2/application/routes.py
@@ -2,11 +2,11 @@ from application import app
 from flask import request, Response
 from random import randint
 
-@app.route('/get/color', methods=['GET'])
-def get_color():
-    animals = ['red', 'white', 'green', 'orange', 'black']
-    animal = animals[randint(0,3)]
-    return Response(animal, mimetype='text/plain')
+@app.route('/get/colour', methods=['GET'])
+def get_colour():
+    colours = ['red', 'white', 'green', 'orange', 'black']
+    colour = colours[randint(0,3)]
+    return Response(colour, mimetype='text/plain')
 
 @app.route('/post/color', methods=['POST'])
 def post_color():
