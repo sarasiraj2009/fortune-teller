5bf0511c97dbfc55f2b845231f4fc7820e215c9c
dreops <dreops@andreasandrews.com>
2020-04-09 16:07:47 +0000

    services 1+2 are working

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,6 +1,18 @@
 from flask import render_template
 from application import app
+import requests
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return render_template('home.html', title='Fortune Teller - Home')
+
+@app.route('/fortune', methods=['GET'])
+def fortune():    
+    return render_template('fortune.html', title='Fortune Results')
+
+
+@app.route('/get/colour', methods = ['GET'])
+def colour():
+    colour = requests.get('http://service2:5002/get/colour')
+    return render_template('fortune.html', title='Fortune', colour=colour.text)
+
---
+++ service4/application/routes.py
@@ -0,0 +1,3 @@
+from application import app
+from Flask import request, Response
+
