44634855b928d0baa82a5eb3891a2c104ceb6794
nidakhawar <nidakhawar@yahoo.com>
2020-04-09 11:44:08 +0000

    more work and changes to service3

--- service3/application/routes.py
+++ service3/application/routes.py
@@ -6,7 +6,3 @@ def number():
     numbers = random.randint(1,8)
     return Response(numbers[randint(0,7)], mimetype='text/plain')
 
-@app.route('/number', methods = ['POST'])
-def number_fortune():
-    number = request.data.decode("utf-8")
-    return Response(number, mimetype='text/plain')
