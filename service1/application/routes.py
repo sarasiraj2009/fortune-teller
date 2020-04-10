304df6d3b996da5908cf1f54da84d17ee9d0226e
dreops <dreops@andreasandrews.com>
2020-04-10 16:10:39 +0100

    changed the python code

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -6,17 +6,18 @@ import requests
 def home():
     return render_template('home.html', title='Fortune Teller - Home')
 
-@app.route('/fortune', methods=['GET'])
+# We dont' need the code below - PP
+""" @app.route('/fortune', methods=['GET'])
 def fortune():    
     return render_template('fortune.html', title='Fortune Results')
-
+ """
 
 @app.route('/get/colour', methods = ['GET'])
 def colour():
-    colour = requests.get('http://service2:5002/get/colour')
-    return render_template('fortune.html', title='Fortune', colour=colour.text)
+    colour = requests.get('http://service2:5002/selection/colour')
+    return render_template('fortune.html', title='Fortunes', colour=colour.text)
 
 @app.route('/get/number', methods = ['GET'])
 def number():
-    number = requests.get('http://service3:5003/get/number')
+    number = requests.get('http://service3:5003/selection/number')
     return render_template('fortune.html', title='Fortune', number=number.text)
--- service2/application/routes.py
+++ service2/application/routes.py
@@ -1,19 +1,23 @@
-# Service 2 Routes by Sara
+# Service 2 Routes by Sara edited by PP
 
 from application import app
 from flask import request, Response
 from random import randint
 
-@app.route('/get/colour', methods=['GET'])
+@app.route('/selection/colour', methods=['GET'])
 def get_colour():
     colours = ['red', 'white', 'green', 'orange', 'black']
-    colour = colours[randint(0,4)]
-    return colour
+ #  colour = colours[randint(0,4)]
+    return Response(colour[randint(0,4)], mimetype='text/plain')
  #  return Response(colour, mimetype='text/plain')
 
-@app.route('/post/colour', methods=['POST'])
+# """ We don't need this - PP
+""" @app.route('/post/colour', methods=['POST'])
 def post_colour():
     return Response(request.data.decode("utf-8"), mimetype='text/plain')
+ """
 
+#  start testing - PP
 colour = get_colour()
 print(colour)
+#  end testing - PP
