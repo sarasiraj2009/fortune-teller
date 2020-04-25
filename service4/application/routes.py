264189e3d1368288b8d7b1ad3ea3d592959bb6b8
dreops <dreops@andreasandrews.com>
2020-04-25 17:04:42 +0100

    service 4 update

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -21,6 +21,8 @@ def sentence():
             result = "You'll have a red1 day"
     #elif colour == "white":
 
-    send_result = {"result": result}        
-    return send_result
+ """    send_result = {"result": result}        
+    return send_result """
+
+    return result
 
