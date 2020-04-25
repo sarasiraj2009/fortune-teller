721180d0d51e6584511cae8ce2988bf6932a4a9e
dreops <dreops@andreasandrews.com>
2020-04-25 16:28:09 +0100

    service 4 update

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -17,7 +17,7 @@ def colour():
     colour = requests.get('http://service2:5002/selection/colour')
     number = requests.get('http://service3:5003/selection/number')
     sentence = requests.get('http://service4:5004/selection/sentence', json={"colour": colour.text, "number": number.text})
-    return render_template('fortune.html', title='Fortunes', colour=colour.text, number=number.text)
+    return render_template('fortune.html', title='Fortunes', colour=colour.text, number=number.text, sentence=sentence)
 
     
 
