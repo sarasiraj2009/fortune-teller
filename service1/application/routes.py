e625ff9276ecbb7cd75ca6c6f13cad7a9ac6a1d5
dreops <dreops@andreasandrews.com>
2020-04-25 15:51:15 +0100

    service 4 first attempt

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -16,7 +16,10 @@ def fortune():
 def colour():
     colour = requests.get('http://service2:5002/selection/colour')
     number = requests.get('http://service3:5003/selection/number')
+    sentence = requests.get('http://service4:5004/selection/sentence', json={"colour": colour.text, "number": number.text})
     return render_template('fortune.html', title='Fortunes', colour=colour.text, number=number.text)
+
+    
 
 # We dont' need the code below - PP
 """ @app.route('/get/number', methods = ['GET'])
--- service4/application/routes.py
+++ service4/application/routes.py
@@ -1,6 +1,9 @@
 from application import app
 from Flask import request, Response
 
-@app.route('/selection/sentence', methods =  ['POST'])
+@app.route('/selection/sentence', methods =  ['POST', 'GET'])
 def sentence():
     sentences = ['You are a winner!', 'Fortune 2','Fortune 3','Fortune 4','Fortune 5','Fortune 6','Fortune 7','Fortune 8']
+    fortune = requests.get('http://service1:5000/selection/sentence', json={"colour": colour.text, "number": number.text})
+    if colour == "green":
+        if number == "1":
