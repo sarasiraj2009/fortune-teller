9b4c4d2e0f3130dc0c2b727dc6495777b237ce9e
dreops <dreops@andreasandrews.com>
2020-04-25 14:21:04 +0100

    service 4 changes from last session

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -1,3 +1,6 @@
 from application import app
 from Flask import request, Response
 
+@app.route('/selection/sentence', methods =  ['POST'])
+def sentence():
+    sentences = ['You are a winner!', 'Fortune 2','Fortune 3','Fortune 4','Fortune 5','Fortune 6','Fortune 7','Fortune 8']
